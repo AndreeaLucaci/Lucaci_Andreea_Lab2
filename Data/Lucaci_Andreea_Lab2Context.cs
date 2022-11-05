1570b3c5eebf7ad104d2234525cc12f68ad3b60d
AndreeaLucaci <andreeateodoralucaci@gmail.com>
2022-11-06 00:19:34 +0200

    Add project files.

---
+++ Data/Lucaci_Andreea_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lucaci_Andreea_Lab2.Models;
+
+namespace Lucaci_Andreea_Lab2.Data
+{
+    public class Lucaci_Andreea_Lab2Context : DbContext
+    {
+        public Lucaci_Andreea_Lab2Context (DbContextOptions<Lucaci_Andreea_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lucaci_Andreea_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Lucaci_Andreea_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Lucaci_Andreea_Lab2.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Create.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Lucaci_Andreea_Lab2.Data;
+using Lucaci_Andreea_Lab2.Models;
+
+namespace Lucaci_Andreea_Lab2.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Lucaci_Andreea_Lab2.Data.Lucaci_Andreea_Lab2Context _context;
+
+        public CreateModel(Lucaci_Andreea_Lab2.Data.Lucaci_Andreea_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
+"PublisherName");
+
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
